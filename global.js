788573b245d05f3d6fdace64d7527728376a6a5e
SummerGarvais <garvaiskevin@gmail.com>
2026-04-21 13:17:54 -0700

    removing nav

--- global.js
+++ global.js
@@ -4,8 +4,33 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
 
-let currentLink = navLinks.find( (a) =>
-    a.host === location.host && a.pathname === location.pathname);
-currentLink?.classList.add('current');
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find( (a) =>
+    // a.host === location.host && a.pathname === location.pathname);
+// currentLink?.classList.add('current');
+
+let pages = [
+  {url: '', title: 'Home'},
+  {url: 'projects/', title: 'Projects'},
+  {url: 'portfolio/', title: 'Portfolio'},
+  {url: 'contact/', title: 'Contact'},
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+
+for (let p of pages){
+  let url = p.url;
+  let title = p.title;
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
