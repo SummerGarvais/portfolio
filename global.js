eb027b2f1783d3b558ab78ae34e2e64282a0882f
SummerGarvais <garvaiskevin@gmail.com>
2026-04-21 13:44:15 -0700

    Update Global.js

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-console.log('IT’S ALIVE!');
+console.log("IT'S ALIVE!");
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
@@ -14,7 +14,7 @@ function $$(selector, context = document) {
 let pages = [
   {url: '', title: 'Home'},
   {url: 'projects/', title: 'Projects'},
-  {url: 'portfolio/', title: 'Portfolio'},
+  {url: 'profile/', title: 'Profile'},
   {url: 'contact/', title: 'Contact'},
 ];
 
@@ -23,7 +23,7 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
-  : "/website/";         // GitHub Pages repo name
+  : "/portfolio/";         // GitHub Pages repo name
 
 
 for (let p of pages){
@@ -32,5 +32,11 @@ for (let p of pages){
   if (!url.startsWith('http')) {
     url = BASE_PATH + url;
   }
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+  if (a.host === location.host && a.pathname.replace(/\/$/, '') === location.pathname.replace(/\/$/, '')) {
+    a.classList.add('current');
+  }
 }
